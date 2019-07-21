47c85c8ed718b95aa763b992f01592079f0347d8
Andreas Koutsoukos <andreas.koutsoukos@gmail.com>
2019-07-21 12:45:36 +0300

    added firebase function export

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,6 @@
 const functions = require('firebase-functions');
+const { Nuxt } = require('nuxt-start');
+const nuxtConfig = require('./nuxt.config.js');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +8,13 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+
+const config = {
+    ...nuxtConfig,
+    debug: false,
+    buildDir: 'nuxt',
+};
+const nuxt = new Nuxt(config);
+  
+exports.ssrapp = functions.https.onRequest((req, res) => nuxt.render(req, res));
